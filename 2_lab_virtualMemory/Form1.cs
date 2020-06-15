66d58f9ee2208eba8e8cdc4433eaf9256af142fb
Artemy Krupsky <4xel1337@gmail.com>
2020-06-15 21:37:11 +0300

    v2.0

--- 2_lab_virtualMemory/Form1.cs
+++ 2_lab_virtualMemory/Form1.cs
@@ -18,149 +18,177 @@ namespace _2_lab_virtualMemory
             InitializeComponent();
         }
 
+        int faultFIFO = 0;
+        int faultLRU = 0;
+        string[] fifoEl = null;
+        string[] lruEl = null;
+        string text;
+        
+
         private void buttonFile_Click(object sender, EventArgs e)
         {
-            int faultFIFO = 0;
-            int faultLRU = 0;
-            string[] FIFOElems = null;
-            string[] LRUElems = null;
-            string text;
-            textBoxFifo.Text = "";
-            textBoxLru.Text = "";
-
+            
             if (ofd.ShowDialog() == DialogResult.OK)
             {
 
-                string filename = ofd.FileName;
-
-                try
-                {
-                    using (var stream = new StreamReader(filename, Encoding.UTF8))
-                    {
-                        text = stream.ReadToEnd();
-                        textBoxDefault.Text = "Your file:" + Environment.NewLine + text + Environment.NewLine;
-                        FIFOElems = new string[text.Length];
-                        LRUElems = new string[text.Length];
-                        FIFOElems = text.Split(' ');
-                        LRUElems = text.Split(' ');
-                    }
-
-                    for (int i = Decimal.ToInt32(frameCount.Value); i != FIFOElems.Length; i++)
-                    {
-                        bool pageFall = true;
-
-                        for (int j = 0; j != Decimal.ToInt32(frameCount.Value); j++)
-                        {
-                            textBoxFifo.Text += FIFOElems[j] + " ";
-
-                            if (FIFOElems[j] == FIFOElems[i])
-                            {
-                                pageFall = false;
-                            }
-                        }
-
-                        if (pageFall == false)
-                        {
-                            textBoxFifo.Text += Environment.NewLine + "No pagefaults found";
-                        }
-
-                        textBoxFifo.Text += Environment.NewLine;
-
-                        if (pageFall == true)
-                        {
-                            textBoxFifo.Text += "Drop: " + FIFOElems[0] + "  Add:" + FIFOElems[i] + Environment.NewLine;
-                            faultFIFO++;
-
-                            for (int k = 0; k < Decimal.ToInt32(frameCount.Value) - 1; k++)
-                            {
-                                FIFOElems[k] = FIFOElems[k + 1];
-                            }
-
-                            FIFOElems[Decimal.ToInt32(frameCount.Value) - 1] = FIFOElems[i];
-                        }
-                    }
-
-                    for (int j = 0; j != Decimal.ToInt32(frameCount.Value); j++)
-                    {
-                        textBoxFifo.Text += FIFOElems[j] + " ";
-                    }
-
-                    textBoxFifo.Text += Environment.NewLine + "All drops:  " + Convert.ToString(faultFIFO);
-
-                    for (int i = Decimal.ToInt32(frameCount.Value); i != LRUElems.Length; i++)
-                    {
-                        for (int j = 0; j != Decimal.ToInt32(frameCount.Value); j++)
-                        {
-                            textBoxLru.Text += LRUElems[j] + " ";
-                        }
-
-                        bool pageFault = true;
-
-                        for (int j = 0; j != Decimal.ToInt32(frameCount.Value); j++)
-                        {
-                            if (LRUElems[j] == LRUElems[i])
-                            {
-                                pageFault = false;
-
-                                for (int k = j; k != Decimal.ToInt32(frameCount.Value) - 1; k++)
-                                {
-                                    LRUElems[k] = LRUElems[k + 1];
-                                }
-
-                                LRUElems[Decimal.ToInt32(frameCount.Value) - 1] = LRUElems[i];
-                            }
-                        }
-
-                        if (pageFault == false)
-                        {
-                            textBoxLru.Text += Environment.NewLine + "Move values";
-                        }
-
-                        textBoxLru.Text += Environment.NewLine;
-
-                        if (pageFault == true)
-                        {
-                            textBoxLru.Text += "Drop: " + LRUElems[0] + "  Add:" + LRUElems[i] + Environment.NewLine;
-                            faultLRU++;
-
-                            for (int k = 0; k < Decimal.ToInt32(frameCount.Value) - 1; k++)
-                            {
-                                LRUElems[k] = LRUElems[k + 1];
-                            }
-
-                            LRUElems[Decimal.ToInt32(frameCount.Value) - 1] = LRUElems[i];
-                        }
-                    }
-                    for (int j = 0; j != Decimal.ToInt32(frameCount.Value); j++)
-                    {
-                        textBoxLru.Text += LRUElems[j] + " ";
-                    }
-
-                    textBoxLru.Text += Environment.NewLine + "All drops:  " + Convert.ToString(faultLRU);
-                    textBoxDefault.Text += "Drops in FIFO: " + Convert.ToString(faultFIFO) + Environment.NewLine +
-                       "Drops in LRU: " + Convert.ToString(faultLRU) + Environment.NewLine;
-
-                    if (faultFIFO < faultLRU)
-                    {
-                        textBoxDefault.Text += "FIFO algorithm is better here.";
-                    }
-
-                    else
-                    {
-                        textBoxDefault.Text += "LRU algorithm is better here.";
-                    }
-                }
-
-                catch
-                {
-                    MessageBox.Show("Incorrect data");
-                }
-
+                input();
+                fifo();
+                lru();
+                output();
             }
 
             else
             {
-                MessageBox.Show("Unable to read file");
+                MessageBox.Show("Unable to read file", "Error");
+            }
+        }
+
+        private void input()
+        {
+            string filename = ofd.FileName;
+
+            textBoxFifo.Text = "";
+            textBoxLru.Text = "";
+
+            text = File.ReadAllText(filename);
+            textBoxDefault.Text = "Selected file:" + Environment.NewLine + text + Environment.NewLine;
+
+            fifoEl = new string[text.Length];
+            lruEl = new string[text.Length];
+
+            fifoEl = text.Split(' ');
+            lruEl = text.Split(' ');
+        }
+
+        private void fifo()
+        {
+            try
+            {
+                for (int count = Convert.ToInt32(frameCount.Value); count != fifoEl.Length; count++)
+                {
+                    bool pageFall = true;
+
+                    for (int j = 0; j != Convert.ToInt32(frameCount.Value); j++)
+                    {
+                        textBoxFifo.Text += fifoEl[j] + " ";
+
+                        if (fifoEl[j] == fifoEl[count])
+                        {
+                            pageFall = false;
+                        }
+                    }
+
+                    if (pageFall == false)
+                    {
+                        textBoxFifo.Text += Environment.NewLine + "No pagefaults found";
+                    }
+
+                    textBoxFifo.Text += Environment.NewLine;
+
+                    if (pageFall == true)
+                    {
+                        textBoxFifo.Text += "Drop: " + fifoEl[0] + "  Add:" + fifoEl[count] + Environment.NewLine;
+                        faultFIFO++;
+
+                        for (int num = 0; num < Convert.ToInt32(frameCount.Value) - 1; num++)
+                        {
+                            fifoEl[num] = fifoEl[num + 1];
+                        }
+
+                        fifoEl[Convert.ToInt32(frameCount.Value) - 1] = fifoEl[count];
+                    }
+                }
+
+                for (int count = 0; count != Convert.ToInt32(frameCount.Value); count++)
+                {
+                    textBoxFifo.Text += fifoEl[count] + " ";
+                }
+
+                textBoxFifo.Text += Environment.NewLine + "All drops: " + Convert.ToString(faultFIFO);
+            }
+
+            catch
+            { 
+                MessageBox.Show("Incorrect data", "Error"); 
+            }
+        }
+
+        private void lru()
+        {
+            try
+            {
+                for (int count = Convert.ToInt32(frameCount.Value); count != lruEl.Length; count++)
+                {
+                    for (int num = 0; num != Convert.ToInt32(frameCount.Value); num++)
+                    {
+                        textBoxLru.Text += lruEl[num] + " ";
+                    }
+
+                    bool pageFault = true;
+
+                    for (int num = 0; num != Convert.ToInt32(frameCount.Value); num++)
+                    {
+                        if (lruEl[num] == lruEl[count])
+                        {
+                            pageFault = false;
+
+                            for (int k = num; k != Convert.ToInt32(frameCount.Value) - 1; k++)
+                            {
+                                lruEl[k] = lruEl[k + 1];
+                            }
+
+                            lruEl[Convert.ToInt32(frameCount.Value) - 1] = lruEl[count];
+                        }
+                    }
+
+                    if (pageFault == false)
+                    {
+                        textBoxLru.Text += Environment.NewLine + "Move values";
+                    }
+
+                    textBoxLru.Text += Environment.NewLine;
+
+                    if (pageFault == true)
+                    {
+                        textBoxLru.Text += "Drop: " + lruEl[0] + "  Add:" + lruEl[count] + Environment.NewLine;
+                        faultLRU++;
+
+                        for (int num = 0; num < Convert.ToInt32(frameCount.Value) - 1; num++)
+                        {
+                            lruEl[num] = lruEl[num + 1];
+                        }
+
+                        lruEl[Convert.ToInt32(frameCount.Value) - 1] = lruEl[count];
+                    }
+                }
+
+                for (int j = 0; j != Convert.ToInt32(frameCount.Value); j++)
+                {
+                    textBoxLru.Text += lruEl[j] + " ";
+                }
+
+                textBoxLru.Text += Environment.NewLine + "All drops: " + Convert.ToString(faultLRU);
+            }
+
+            catch
+            {
+                MessageBox.Show("Incorrect data", "Error");
+            }
+        }
+
+        private void output()
+        {
+            textBoxDefault.Text += "Drops in FIFO: " + Convert.ToString(faultFIFO) + Environment.NewLine +
+                       "Drops in LRU: " + Convert.ToString(faultLRU) + Environment.NewLine;
+
+            if (faultFIFO < faultLRU)
+            {
+                textBoxDefault.Text += "FIFO algorithm is better here.";
+            }
+
+            else
+            {
+                textBoxDefault.Text += "LRU algorithm is better here.";
             }
         }
     }
